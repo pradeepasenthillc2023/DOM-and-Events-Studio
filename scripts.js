aebcf4f1be868894ddd6912d5a56d99a53687149
PRADEEPA SENTHILRAJAN <pradeepa.senthil@gmail.com>
2023-07-13 20:02:38 -0500

    Done

--- scripts.js
+++ scripts.js
@@ -1,2 +1,10 @@
+function init() {
+    let takeOffButton = document.getElementById("takeoff");
+    takeoffButton.addEventListener("click",function(event) {
+    console. log ("confirms that shuttle is ready to takeoff!");
+    });
+} window.addEventListener('load',init)
+
+
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
